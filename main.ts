13ef2236d567d444a3700775b4b5f09eaf40dcf3
garfbit <104676660+garfbit@users.noreply.github.com>
2022-05-14 14:53:51 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
 basic.forever(function () {
     if (input.acceleration(Dimension.X) == 0) {
         basic.showIcon(IconNames.No)
-    } else if (input.acceleration(Dimension.X) > 0) {
+    } else if (input.acceleration(Dimension.X) > 20) {
         basic.showArrow(ArrowNames.East)
-    } else if (input.acceleration(Dimension.X) < 0) {
+    } else if (input.acceleration(Dimension.X) < -20) {
         basic.showArrow(ArrowNames.West)
     }
     if (input.acceleration(Dimension.Y) == 0) {
         basic.showIcon(IconNames.No)
-    } else if (input.acceleration(Dimension.Y) < 0) {
-        basic.showArrow(ArrowNames.North)
-    } else if (input.acceleration(Dimension.Y) > 0) {
+    } else if (input.acceleration(Dimension.Y) > 20) {
         basic.showArrow(ArrowNames.South)
+    } else if (input.acceleration(Dimension.Y) < -20) {
+        basic.showArrow(ArrowNames.North)
     }
 })
